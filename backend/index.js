af91ebe08febbdf086a7a3927e472e5b974756e3
chaitanyapandey444 <139068441+chaitanyapandey444@users.noreply.github.com>
2024-11-06 17:40:17 +0530

    cloudinary banaya

--- backend/index.js
+++ backend/index.js
@@ -2,10 +2,16 @@ import express from 'express';
 import dotenv from 'dotenv';
 import connectDb from './database/db.js';
 import cookieParser from 'cookie-parser';
+import cloudinary from "cloudinary";
 dotenv.config();
 
-const app = express();
 
+cloudinary.v2.config({
+    cloud_name: process.env.Cloud_Name,
+    api_key: process.env.Cloud_Api,
+    api_secret: process.env.Cloud_Secret,
+  });
+  const app = express();
 // using middlewares
 app.use(express.json());
 app.use(cookieParser()); //can read token from cookie
